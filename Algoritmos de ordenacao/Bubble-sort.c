7c165ca02c11713e5d31252057becd25ac0dca15
Phsarmento <pedrohenriquesarmento@gmail.com>
2023-09-28 20:56:49 -0300

    Bubble sort finalizado

--- Algoritmos de ordenacao/Bubble-sort.c
+++ Algoritmos de ordenacao/Bubble-sort.c
@@ -1,6 +1,5 @@
 // Este algoritmo é por comparação e do grupo dos menos eficientes.
 // Bubble Sort
-// Coloca o maior no ultimo e vai diminuindo na bolha
 
 #include <stdio.h>
 
@@ -13,13 +12,14 @@ int main(){
     int vetor[7] = {9,8,1,2,0,4,7};
     int maior=0,num,bolha = 6;
 
-    for(int i = 0; i < bolha ; i++){
-        for(int t = 1; t < bolha ; t++){
-            if(vetor[i] < vetor[t]) maior = t;   
+    for(int i = 0; i < 7 ; i++){
+        for(int t = 0 ; t < bolha; t++){
+            if(vetor[t] > vetor[t + 1]){
+                num = vetor[t];
+                vetor[t] = vetor[t+1];
+                vetor[t + 1] = num;
+            }
         }
-        num = vetor[bolha];
-        vetor[bolha] = vetor[maior];
-        vetor[maior] = num;
         bolha--;
     }
     print_vetor(vetor,7);
